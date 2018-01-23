82ef729854b267000e93b96a193a21a7ac6d0b8f
William Buttenham <connrobu@gmail.com>
2018-01-23 01:34:25 -0500

    Added light status for third button. Third button is used as layer select for first two. Both trigger CRTL+` on the first layer and the first button is for a password, the second for windows locking.

--- macro-keyboard.ino
+++ macro-keyboard.ino
@@ -1,10 +1,17 @@
 #include "Keyboard.h"
 
+// THE PRO MICRO IN THE MACRO PAD IS 3V
+int RXLED = 17;
+
 void setup() {
   // make pin an input and turn on the pull-up resistor so it goes high unless connected to ground
   pinMode(7, INPUT_PULLUP);
   pinMode(8, INPUT_PULLUP);
-  // pinMode(9, INPUT_PULLUP);
+  pinMode(9, INPUT_PULLUP);
+
+  pinMode(RXLED, OUTPUT);
+  digitalWrite(RXLED, HIGH);
+  
   Keyboard.begin();
 }
 
@@ -12,30 +19,32 @@ void loop() {
   // Check the switches:
   int buttonState7 = digitalRead(7);
   int buttonState8 = digitalRead(8);
-  // int buttonState9 = digitalRead(9);
+  int buttonState9 = digitalRead(9);
 
-  if (buttonState7 == LOW) {
-    Keyboard.write('*');
-    Keyboard.write('*');
-    Keyboard.write('*');
-    Keyboard.write('*');
-    Keyboard.write('*');
-    Keyboard.write('*');
-    Keyboard.write('*');
-    Keyboard.write('*');
-    Keyboard.write('*');
-    Keyboard.write('*');
-    delay(1000);
+  if (buttonState9 == HIGH) {
+    digitalWrite(RXLED, LOW);
+    TXLED1;
+  } else {
+    digitalWrite(RXLED, HIGH);
+    TXLED0;
   }
 
-  if (buttonState8 == LOW) {
+  if (buttonState7 == LOW && buttonState9 == LOW) {
+    Keyboard.press(KEY_LEFT_CTRL);
+    Keyboard.press('`');
+    Keyboard.releaseAll();
+  } else if (buttonState7 == LOW && buttonState9 == HIGH) {
+    Keyboard.print("******");
+    delay(300);
+  }
+
+  if (buttonState8 == LOW && buttonState9 == LOW) {
+    Keyboard.press(KEY_LEFT_CTRL);
+    Keyboard.press('`');
+    Keyboard.releaseAll();
+  } else if (buttonState8 == LOW && buttonState9 == HIGH) {
     Keyboard.press(KEY_LEFT_GUI);
     Keyboard.press('l');
     Keyboard.releaseAll();
-    delay(1000);
   }
-
-  // if (buttonState9 == HIGH) {
-  // }
-  delay(1000);
 }
